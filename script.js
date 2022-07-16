18ee05267f2a93a36f63eb3ba845b0c71dce7850
Arthur <artyruakobhuk@gmail.com>
2022-07-16 11:56:15 +0300

    update homeworks files

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
 
-const input = document.querySelector('#films');
-const button = document.querySelector('#tick');
-const span = document.querySelector('.text');
-const films = document.querySelector('.filmes');
-const arr = [];
-
-
-input.addEventListener('input', () => {
-	if(input.value.length < 50) {
-		span.innerHTML = `We did it ${input.value.length}`;
-	} else if (input.value.length > 50) {
-		span.innerHTML = `Вы ввели  ${input.value.length} символов, слишком длинное название`;
-	}
-	console.log(input.value.length)
-	if(input.value.length < 1){
-		span.innerHTML = '';
-		films.remove();
+function sortStudentsByGroups(arr) {
+ const students = arr.sort();
+ const stud1 = students.slice(0, 3);
+ const stud2 = students.slice(3, 6);
+ const stud3 = students.slice(6, 9);
+ const others = students.length == 9 ? '-' : students.slice(9, students.length).join(', ');
+ const res = [stud1, stud2, stud3, `Оставшиеся студенты: ${others}`];
+ return res;
 }
-})
-button.addEventListener('click', () => {
-	arr.push(input.value);
-	console.log(arr);
-		films.innerHTML = `<div class="filmes_item">Фильм ${arr} под номером </div>`;
-})
 
+sortStudentsByGroups(students);
